c46e49247eefd4751abb6229e15d1fa5506d5946
Greg Korte <gregmkorte@gmail.com>
2024-06-03 15:16:54 -0500

    Update project_template_mac.sh for view template, seed_database script and db.sqlite3 file

--- project_template_mac.sh
+++ project_template_mac.sh
@@ -13,6 +13,8 @@ echo 'db.sqlite3' >> .gitignore
 pipenv install django autopep8 pylint djangorestframework django-cors-headers pylint-django
 django-admin startproject ${PROJECT_NAME}project .
 python3 manage.py startapp ${PROJECT_NAME}api
+touch ./db.sqlite3
+touch ./seed_database.sh
 mkdir -p ./.vscode
 mkdir -p ./${PROJECT_NAME}api/fixtures
 touch ./${PROJECT_NAME}api/fixtures/users.json
@@ -22,6 +24,7 @@ touch ./${PROJECT_NAME}api/models/__init__.py
 mkdir -p ./${PROJECT_NAME}api/views
 touch ./${PROJECT_NAME}api/views/__init__.py
 touch ./${PROJECT_NAME}api/views/auth.py
+touch ./${PROJECT_NAME}api/views/template.py
 rm ./${PROJECT_NAME}api/views.py
 rm ./${PROJECT_NAME}api/models.py
 
@@ -252,6 +255,120 @@ def register_user(request):
         'email': new_user.email
     }
     return Response(data)
+EOL
+
+cat <<EOL > ./${PROJECT_NAME}api/views/template.py
+from django.http import HttpResponseServerError
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from rockapi.models import Void
+
+
+class TemplateView(ViewSet):
+    """Void view set"""
+
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized instance
+        """
+        void = Void()
+        void.sample_name = request.data["name"]
+        void.sample_description = request.data["description"]
+
+        try:
+            void.save()
+            serializer = VoidSerializer(void)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as ex:
+            return Response({"reason": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single item
+
+        Returns:
+            Response -- JSON serialized instance
+        """
+        try:
+            void = Void.objects.get(pk=pk)
+            serializer = VoidSerializer(void)
+            return Response(serializer.data)
+        except Exception as ex:
+            return Response({"reason": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        try:
+            void = Void.objects.get(pk=pk)
+            void.sample_name = request.data["name"]
+            void.sample_description = request.data["description"]
+            void.save()
+        except Void.DoesNotExist:
+            return Response(None, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            void = Void.objects.get(pk=pk)
+            void.delete()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+        except Void.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        """Handle GET requests for all items
+
+        Returns:
+            Response -- JSON serialized array
+        """
+        try:
+            voids = Void.objects.all()
+            serializer = VoidSerializer(voids, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+
+class VoidSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+
+    class Meta:
+        model = Void
+        fields = ( 'id', 'sample_name', 'sample_description', )
+
+EOL
+
+cat <<EOL > ./seed_database.sh
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./${PROJECT_NAME}api/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations ${PROJECT_NAME}api
+python3 manage.py migrate ${PROJECT_NAME}api
+python3 manage.py loaddata user
+python3 manage.py loaddata token
+
 EOL
 
 pipenv run python3 manage.py makemigrations
