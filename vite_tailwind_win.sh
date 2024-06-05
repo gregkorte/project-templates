8cc87e313ca12cd0f58f3ff111bf7facafc05f48
Greg Korte <gregmkorte@gmail.com>
2024-06-05 15:31:36 -0500

    Update vite_tailwind_win.sh, change single quotes to double

--- vite_tailwind_win.sh
+++ vite_tailwind_win.sh
@@ -264,8 +264,8 @@ export const App = () => {
 
     return (
         <Routes>
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
             <Route path="*" element={
                     <Authorized>
