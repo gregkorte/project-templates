8b391785b4826a984849f2fd77ad11a22343873c
Greg Korte <gregmkorte@gmail.com>
2024-06-05 15:39:11 -0500

    Update vite_tailwind_win.sh, change master to main

--- vite_tailwind_win.sh
+++ vite_tailwind_win.sh
@@ -280,7 +280,7 @@ export const App = () => {
 
 git init
 git remote add origin ${REPO_NAME}
-git branch -M main
+git branch -m master main
 git add .
 git commit -m "Initial commit"
 git push -u origin main
