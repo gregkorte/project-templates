e9ac6876c5ee6967dae21fbef2e5d8cb45756165
Greg Korte <gregmkorte@gmail.com>
2024-07-20 11:55:29 -0500

    Update vite_tailwind_win.sh
    Fixed import link

--- vite_tailwind_win.sh
+++ vite_tailwind_win.sh
@@ -427,7 +427,7 @@ cat <<EOL > ./src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ApplicationViews from './components/ApplicationViews.jsx'
-import '.index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
