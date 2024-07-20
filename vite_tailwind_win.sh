78005702aee85d12eef053b41a10fe7a41655b41
Greg Korte <gregmkorte@gmail.com>
2024-07-20 02:14:15 -0500

    Update vite_tailwind_win.sh
    Commenting out add, commit, push for testing

--- vite_tailwind_win.sh
+++ vite_tailwind_win.sh
@@ -282,10 +282,10 @@ export default App
 
 git init
 git checkout -b main
-git remote add origin ${REPO_NAME}
-git add .
-git commit -m "Initial commit"
-git push -u origin main
+# git remote add origin ${REPO_NAME}
+# git add .
+# git commit -m "Initial commit"
+# git push -u origin main
 
 echo "**********************************"
 echo ""
