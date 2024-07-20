0e9a7866755c7f3e0daad157968e953967337a3c
Greg Korte <gregmkorte@gmail.com>
2024-07-20 11:22:44 -0500

    Update vite_tailwind_win.sh
    Changing userServices Authorization token in hopes that it will render correctly. Adding back default App.jsx and removing index.css tailwind replacement code (will add back in to preserve original index.css from react install

--- vite_tailwind_win.sh
+++ vite_tailwind_win.sh
@@ -40,17 +40,17 @@ export default {
 }
 EOL
 
-cat <<EOL > ./src/index.css
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-EOL
+# cat <<EOL > ./src/index.css
+# @tailwind base;
+# @tailwind components;
+# @tailwind utilities;
+# EOL
 
 cat <<EOL > ./src/components/services/userServices.jsx
 export const getCurrentUser = () => {
     return fetch("http://localhost:8000/current_user", {
         headers: {
-            Authorization: "Token + " " + JSON.parse(localStorage.getItem('${PROJECT_NAME}_token')).token",
+            Authorization: "Token " + JSON.parse(localStorage.getItem('${PROJECT_NAME}_token')).token,
             "Content-Type": "application/json"
         }
     }).then(res => res.json())
@@ -165,8 +165,8 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Login.css"
 
 export const Login = () => {
-    const [email, setEmail] = useState("steve@brownlee.com")
-    const [password, setPassword] = useState("brownlee")
+    const [email, setEmail] = useState("tyler@hilliard.com")
+    const [password, setPassword] = useState("hilliard")
     const existDialog = useRef()
     const navigate = useNavigate()
 
@@ -199,7 +199,7 @@ export const Login = () => {
 
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1 className="text-4xl mt-7 mb-3">${PROJECT_NAME}</h1>
+                    <h1 className="text-4xl mt-7 mb-3">bandaide</h1>
                     <h2 className="text-xl mb-10">Please sign in</h2>
                     <fieldset className="mb-4">
                         <label htmlFor="inputEmail"> Email address </label>
@@ -276,28 +276,38 @@ export const ApplicationViews = () => {
 EOL
 
 cat <<EOL > ./src/App.jsx
-import { Route, Routes } from "react-router-dom"
-import "./App.css"
-import { Login } from "./components/auth/Login.jsx"
-import { Register } from "./components/auth/Register.jsx"
-import { ApplicationViews } from "./components/views/ApplicationViews.jsx"
-import { Authorized } from "./components/views/Authorized.jsx"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-
-            <Route path="*" element={
-                    <Authorized>
-                        <ApplicationViews />
-                    </Authorized>
-                }
-            />
-        </Routes>
-      )
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
