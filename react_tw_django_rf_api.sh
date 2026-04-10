237376ac7c2d1d07dfb369c3c29e3850a2d1f09b
Greg Korte <gregmkorte@gmail.com>
2026-04-10 13:44:39 -0500

    Update react_tw_django_rf_api.sh
    Adding checkpoints to pipenv scripts that are currently failing.

--- react_tw_django_rf_api.sh
+++ react_tw_django_rf_api.sh
@@ -11,8 +11,10 @@ read -p "> " REPO_NAME
 curl -L -s 'https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore' > .gitignore
 echo 'db.sqlite3' >> .gitignore
 pipenv install django autopep8 pylint djangorestframework django-cors-headers pylint-django
-django-admin startproject ${PROJECT_NAME}project .
-python3 manage.py startapp ${PROJECT_NAME}api
+pipenv run django-admin startproject "${PROJECT_NAME}project" . \
+  || { echo "❌ django-admin startproject failed"; exit 1; }
+pipenv run python3 manage.py startapp "${PROJECT_NAME}api" \
+  || { echo "❌ startapp failed"; exit 1; }
 touch ./db.sqlite3
 touch ./seed_database.sh
 mkdir ./.vscode
